da7c244886b15f389f73e5dbce1c9f974deebe6e
wangying <wangying@users.noreply.github.com>
2026-05-06 19:26:27 +0800

    chore(miniprogram): load apiKey from untracked config.local

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -10,11 +10,22 @@ const getEnvVersion = () => {
 
 const envVersion = getEnvVersion();
 
+let localConfig = {};
+try {
+  localConfig = require('./config.local');
+} catch (e) {
+  localConfig = {};
+}
+
 const baseUrlByEnv = {
   develop: 'https://box2bitable-249568-6-1424272965.sh.run.tcloudbase.com',
   trial: 'https://box2bitable-249568-6-1424272965.sh.run.tcloudbase.com',
   release: 'https://box2bitable-249568-6-1424272965.sh.run.tcloudbase.com',
 };
+
+if (localConfig && localConfig.baseUrlByEnv && typeof localConfig.baseUrlByEnv === 'object') {
+  Object.assign(baseUrlByEnv, localConfig.baseUrlByEnv);
+}
 
 const baseUrl = Object.prototype.hasOwnProperty.call(baseUrlByEnv, envVersion)
   ? baseUrlByEnv[envVersion]
@@ -26,6 +37,10 @@ const cloudEnvIdByEnv = {
   release: 'prod-d6gwsf3w487222b9f',
 };
 
+if (localConfig && localConfig.cloudEnvIdByEnv && typeof localConfig.cloudEnvIdByEnv === 'object') {
+  Object.assign(cloudEnvIdByEnv, localConfig.cloudEnvIdByEnv);
+}
+
 const cloudEnvId = Object.prototype.hasOwnProperty.call(cloudEnvIdByEnv, envVersion)
   ? cloudEnvIdByEnv[envVersion]
   : cloudEnvIdByEnv.release;
@@ -36,6 +51,10 @@ const cloudServiceByEnv = {
   release: 'box2bitable',
 };
 
+if (localConfig && localConfig.cloudServiceByEnv && typeof localConfig.cloudServiceByEnv === 'object') {
+  Object.assign(cloudServiceByEnv, localConfig.cloudServiceByEnv);
+}
+
 const cloudService = Object.prototype.hasOwnProperty.call(cloudServiceByEnv, envVersion)
   ? cloudServiceByEnv[envVersion]
   : cloudServiceByEnv.release;
@@ -45,6 +64,10 @@ const apiKeyByEnv = {
   trial: '',
   release: '',
 };
+
+if (localConfig && localConfig.apiKeyByEnv && typeof localConfig.apiKeyByEnv === 'object') {
+  Object.assign(apiKeyByEnv, localConfig.apiKeyByEnv);
+}
 
 const apiKey = Object.prototype.hasOwnProperty.call(apiKeyByEnv, envVersion)
   ? apiKeyByEnv[envVersion]
